3bcc1cbf23a572e6eb979ddce3ade1748934dbd9
lsq <lsqypj@gmail.com>
2019-11-15 20:10:24 +0800

    modify build scripts

--- aria_build.sh
+++ aria_build.sh
@@ -6,7 +6,4 @@ cd  aria2-android
 ls -al
 
 ./build_all.sh
-cp -rf ./bin/armeabi-v7a/bin/aria2c    $APPVEYOR_JOB_ID
-cp -rf ./bin/arm64-v8a/bin/aria2c      $APPVEYOR_JOB_ID
-cp -rf ./bin/armeabi-x86/bin/aria2c    $APPVEYOR_JOB_ID
-cp -rf ./bin/armeabi-x86_64/bin/aria2c $APPVEYOR_JOB_ID
+cp -rf ./bin    $APPVEYOR_JOB_ID/aria2-android
