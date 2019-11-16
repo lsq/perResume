803a60900c1ea9aa32b732f1ecb36207be236cc3
lsq <lsqypj@gmail.com>
2019-11-16 17:05:40 +0800

    add texlive

---
+++ texlive.sh
@@ -0,0 +1,37 @@
+
+basename() {
+    # Usage: basename "path" ["suffix"]
+    local tmp
+
+    #echo "${1##*[!/]}"
+    tmp=${1%"${1##*[!/]}"}
+    tmp=${tmp##*/}
+    tmp=${tmp%"${2/"$tmp"}"}
+
+    printf '%s\n' "${tmp:-/}"
+}
+
+### only for iso file on unix
+#mount -t iso9660 -o ro,loop,noauto /your/texlive.iso /mnt
+
+### for network install
+creat_profile(){
+  touch install_texlive.profile
+}
+download_source(){
+}
+install_texlive(){
+  local sr_url="$1"
+  local tar_file="${sr_url##*/}"
+  #local sr_dir=$(basename "$tar_file" .tar.gz)
+  [ -f "$tar_file" ] && local sr_dir=$(tar tf "$tar_file" | head -1)
+  [ $? -ne 0 -o -n "$sr_dir" ] && exit 1
+  tar xf "$tar_file" && cd "$sr_dir"
+  ./install-tl -profile install_texlive.profile
+  cat tlpkg/texlive.profile
+}
+
+# https://mirror.bjtu.edu.cn/ctan/systems/texlive/tlnet/tlpkg/
+echo "====================="
+echo "begin install........"
+install_texlive http://mirror.ctan.org/systems/texlive/tlnet/install-tl-unx.tar.gz
