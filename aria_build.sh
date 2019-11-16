aee450b059c4e0a41231fc7725044c061ce292c8
lsq <lsqypj@gmail.com>
2019-11-16 17:22:28 +0800

    modify tex build settings

--- aria_build.sh
+++ aria_build.sh
@@ -2,6 +2,9 @@
 
 git clone --recurse-submodules https://github.com/devgianlu/aria2-android.git
 
+wget https://dl.google.com/android/repository/android-ndk-r20-linux-x86_64.zip
+unzip -q android-ndk-r20-linux-x86_64.zip
+
 cd  aria2-android
 ls -al
 
