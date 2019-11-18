7939c7fb5a1171c1221ee2623daf7ef19feac63e
lsq <lsqypj@gmail.com>
2019-11-18 15:35:57 +0800

    modify html to markdown

--- htmltomd.sh
+++ htmltomd.sh
@@ -3,3 +3,4 @@
 
 curl -s https://linuxhandbook.com/sed-reference-guide > sed-reference-guide.html
 pandoc -f html -t markdown -o sed-reference-guide.md sed-reference-guide.html
+[ -f sed-reference-guide.md ] && mv  sed-reference-guide.md $APPVEYOR_BUILD_FOLDER/$APPVEYOR_JOB_ID/
