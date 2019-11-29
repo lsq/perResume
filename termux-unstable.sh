1c99136e5fe0b09ca9599847b0e6adc128e98cf6
lsq <lsqypj@gmail.com>
2019-11-30 01:05:26 +0800

    add termux unstable packages6

--- termux-unstable.sh
+++ termux-unstable.sh
@@ -2,10 +2,7 @@ arch="aarch64"
 package_name=geckodriver
 git clone https://github.com/termux/unstable-packages
 cd ./unstable-packages
-bash -x ./start-builder.sh
 mv disabled-packages/geckodriver/ packages/
 ls -al packages/geckodriver/
-ls -al
-./build-package.sh -a ${arch} ${package_name}
-
+bash -x ./start-builder.sh ./build-package.sh -a ${arch} ${package_name}
 ls -al
