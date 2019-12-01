eac91535fc3b45b92b011e2e128761e8af7fa64b
lsq <lsqypj@gmail.com>
2019-12-01 22:08:18 +0800

    update termux-usb

--- termux-unstable.sh
+++ termux-unstable.sh
@@ -7,5 +7,6 @@ ls -al packages/geckodriver/
 sed -i '/docker exec --interactive --tty/s/--interactive --tty//' start-builder.sh
 source ./start-builder.sh
 docker exec --tty "$CONTAINER_NAME" pwd
+docker exec --tty "$CONTAINER_NAME" ls -al
 #./build-package.sh -a ${arch} ${package_name}
 ls -al
