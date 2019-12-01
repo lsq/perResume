9b20dd8734f89a1ad6b94885a421fabb7472a845
lsq <lsqypj@gmail.com>
2019-12-01 09:53:43 +0800

    Update termux-unstable.sh

--- termux-unstable.sh
+++ termux-unstable.sh
@@ -4,7 +4,7 @@ git clone https://github.com/termux/unstable-packages
 cd ./unstable-packages
 mv disabled-packages/geckodriver/ packages/
 ls -al packages/geckodriver/
-sed -i '/docker exec --interactive --tty/s/--interactive//' start-builder.sh
+sed -i '/docker exec --interactive --tty/s/--interactive --tty//' start-builder.sh
 source ./start-builder.sh
 docker exec --tty "$CONTAINER_NAME" pwd
 #./build-package.sh -a ${arch} ${package_name}
