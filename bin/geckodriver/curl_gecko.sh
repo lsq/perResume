35b646cfacc6122fdfd35b6c94d9c55948783009
lsq <lsqypj@gmail.com>
2019-12-13 07:24:25 +0800

    Update curl_gecko.sh

--- bin/geckodriver/curl_gecko.sh
+++ bin/geckodriver/curl_gecko.sh
@@ -9,7 +9,7 @@ geckodriver &
 curl -d '{"capabilities": {"alwaysMatch": {"acceptInsecureCerts": true}}}' http://localhost:4444/session
 # {"sessionId":"d4605710-5a4e-4d64-a52a-778bb0c31e00","value":{"XULappId":"{ec8030f7-c20a-464f-9b0e-13a3a9e97384}","acceptSslCerts":false,"appBuildId":"20160913030425","browserName":"firefox","browserVersion":"51.0a1","command_id":1,"platform":"LINUX","platformName":"linux","platformVersion":"4.9.0-1-amd64","processId":17474,"proxy":{},"raisesAccessibilityExceptions":false,"rotatable":false,"specificationLevel":0,"takesElementScreenshot":true,"takesScreenshot":true,"version":"51.0a1"}}
 # curl -d '{"url": "https://mozilla.org"}' http://localhost:4444/session/d4605710-5a4e-4d64-a52a-778bb0c31e00/url
-{}
+# {}
 # % curl http://localhost:4444/session/d4605710-5a4e-4d64-a52a-778bb0c31e00/url
 # {"value":"https://www.mozilla.org/en-US/"
 # % curl -X DELETE http://localhost:4444/session/d4605710-5a4e-4d64-a52a-778bb0c31e00
