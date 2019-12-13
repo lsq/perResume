e6d79a0620332e984a04d5d6a7fe477ede0de2f3
lsq <lsqypj@gmail.com>
2019-12-13 08:19:46 +0800

    Update appveyor_prepare.sh

--- appveyor_prepare.sh
+++ appveyor_prepare.sh
@@ -1,8 +1,8 @@
 
 sudo apt-get update
 #sudo apt-get upgrade -y
-sudo apt-get install -y autoconf automake autopoint autotools-dev libtool\
-libxml2-dev libcppunit-dev aria2 pandoc  libcppunit-doc libglib2.0-doc\
-libgraphite2-utils icu-doc texlive-latex-recommended texlive-xetex\
-texlive-luatex pandoc-citeproc texlive-latex-extra context wkhtmltopdf\
+sudo apt-get install -y autoconf automake autopoint autotools-dev libtool \
+libxml2-dev libcppunit-dev aria2 pandoc  libcppunit-doc libglib2.0-doc \
+libgraphite2-utils icu-doc texlive-latex-recommended texlive-xetex \
+texlive-luatex pandoc-citeproc texlive-latex-extra context wkhtmltopdf \
 netstat-nat
