d50f726a95304b15873c497fdca87c08da932528
lsq <lsqypj@gmail.com>
2021-02-19 22:59:53 +0800

    fix bug gcc

--- tensorflow_arm64.sh
+++ tensorflow_arm64.sh
@@ -3,6 +3,7 @@
 git clone https://github.com/lhelontra/tensorflow-on-arm
 cd tensorflow-on-arm/build_tensorflow/
 sed -i '/^RUN git checkout v2.3.0/s/RUN git checkout v2.3.0/RUN git checkout master/' ./Dockerfile.bullseye
+sed -i '15,18s/^/#/'  ./Dockerfile.bullseye
 docker build -t tf-arm -f Dockerfile.bullseye .
 # rpi.conf, rk3399.conf ...
 docker run -i -v /tmp/tensorflow_pkg/:/tmp/tensorflow_pkg/ --env TF_PYTHON_VERSION=3.9 tf-arm ./build_tensorflow.sh configs/rk3399.conf 
