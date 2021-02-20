b01728e5d71fe1eb63ced135c163a234c0b503a7
lsq <lsqypj@gmail.com>
2021-02-21 03:31:54 +0800

    change badh argument

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # my person Resume
+[![Build Status](https://travis-ci.com/lsq/perResume.svg?branch=master)](https://travis-ci.com/lsq/perResume)
 [![Build status](https://ci.appveyor.com/api/projects/status/qa0o9wg3vipkfuuw/branch/master?svg=true)](https://ci.appveyor.com/project/lsq/perresume/branch/master)
--- tensorflow_arm64.sh
+++ tensorflow_arm64.sh
@@ -8,4 +8,4 @@ sed -i "31s@^@RUN sed -i '/unzip/s/unzip/unzip -q/' ./build_tensorflow.sh\n@" ./
 sed -i '15,18s/^/#/'  ./Dockerfile.bullseye
 docker build -t tf-arm -f Dockerfile.bullseye .
 # rpi.conf, rk3399.conf ...
-docker run -i -v /tmp/tensorflow_pkg/:/tmp/tensorflow_pkg/ --env TF_PYTHON_VERSION=3.9 tf-arm ./build_tensorflow.sh configs/rk3399.conf 
+docker run -i -v /tmp/tensorflow_pkg/:/tmp/tensorflow_pkg/ --env TF_PYTHON_VERSION=3.9 tf-arm bash -x ./build_tensorflow.sh configs/rk3399.conf 
