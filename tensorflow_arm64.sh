8b1acf3489e3c909d4da7e8984bea6cf70b3ec4b
lsq <lsqypj@gmail.com>
2021-02-21 03:56:32 +0800

    modified:   tensorflow_arm64.sh

--- tensorflow_arm64.sh
+++ tensorflow_arm64.sh
@@ -5,6 +5,7 @@ cd tensorflow-on-arm/build_tensorflow/
 sed -i '/^RUN git checkout v2.3.0/s/RUN git checkout v2.3.0/RUN git checkout master/' ./Dockerfile.bullseye
 sed -i "31s@^@RUN sed -i '/wget/s/wget/wget -nv/' ./build_tensorflow.sh\n@" ./Dockerfile.bullseye
 sed -i "31s@^@RUN sed -i '/unzip/s/unzip/unzip -q/' ./build_tensorflow.sh\n@" ./Dockerfile.bullseye
+sed -i "31s@^@RUN sed -i '121s/^/bash -x /' ./build_tensorflow.sh\n@" ./Dockerfile.bullseye
 sed -i '15,18s/^/#/'  ./Dockerfile.bullseye
 docker build -t tf-arm -f Dockerfile.bullseye .
 # rpi.conf, rk3399.conf ...
