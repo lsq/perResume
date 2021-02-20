39e7e5f133e7024bb68d48367cdbd850c66d9b53
lsq <lsqypj@gmail.com>
2021-02-20 12:27:27 +0800

    add wget argument

--- tensorflow_arm64.sh
+++ tensorflow_arm64.sh
@@ -3,6 +3,7 @@
 git clone https://github.com/lhelontra/tensorflow-on-arm
 cd tensorflow-on-arm/build_tensorflow/
 sed -i '/^RUN git checkout v2.3.0/s/RUN git checkout v2.3.0/RUN git checkout master/' ./Dockerfile.bullseye
+sed -i "31s@^@RUN sed -i '/wget/s/wget/wget -nv/' ./build_tensorflow.sh\n@" ./Dockerfile.bullseye
 sed -i '15,18s/^/#/'  ./Dockerfile.bullseye
 docker build -t tf-arm -f Dockerfile.bullseye .
 # rpi.conf, rk3399.conf ...
