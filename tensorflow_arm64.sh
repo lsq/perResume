1dc0d31438c821f39cfd85ca6f94a3202d997b57
lsq <lsqypj@gmail.com>
2021-02-21 20:51:25 +0800

    change bazel version

--- tensorflow_arm64.sh
+++ tensorflow_arm64.sh
@@ -6,6 +6,7 @@ sed -i '/^RUN git checkout v2.3.0/s/RUN git checkout v2.3.0/RUN git checkout mas
 sed -i "31s@^@RUN sed -i '/wget/s/wget/wget -nv/' ./build_tensorflow.sh\n@" ./Dockerfile.bullseye
 sed -i "31s@^@RUN sed -i '/unzip/s/unzip/unzip -q/' ./build_tensorflow.sh\n@" ./Dockerfile.bullseye
 sed -i "31s@^@RUN sed -i '121s/^/bash -x /' ./build_tensorflow.sh\n@" ./Dockerfile.bullseye
+sed -i "31s@^@RUN sed -i '/BAZEL_VERSION=/s/=.*$/=\"4.0.0\"/' ./configs/rk3399.conf\n@" ./Dockerfile.bullseye
 sed -i '15,18s/^/#/'  ./Dockerfile.bullseye
 docker build -t tf-arm -f Dockerfile.bullseye .
 # rpi.conf, rk3399.conf ...
