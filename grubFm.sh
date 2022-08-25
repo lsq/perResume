400a7d61da3800fa323cdcac236ff9338860f6f3
lsq <lsqypj@gmail.com>
2022-08-25 14:13:08 +0800

    Update grubFm.sh

--- grubFm.sh
+++ grubFm.sh
@@ -6,7 +6,7 @@ git submodule update --init --recursive
 sudo apt-get install -y gettext grub2-common genisoimage p7zip-full xorriso  mtools
 cd grub2-filemanager
 bash -x ./update_grub2.sh
-sed -i '/cp grubfm/d;/ \+cp/s/^/    cp grubfm\*\.\* releases\/\n/' grubRelease.sh
+sed -i '/cp grubfm/d;/ \+cp/s/^/    cp grubfm\*\.\* releases\/\n/' release.sh
 bash -x ./release.sh
 cp grubfm-zh_CN.7z ../$APPVEYOR_JOB_ID/
 cp grubfm-en_US.7z ../$APPVEYOR_JOB_ID/
