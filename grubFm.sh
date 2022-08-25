d7f9d873e2832addeaf1ee88da2823a4812f6628
lsq <lsqypj@gmail.com>
2022-08-25 09:29:42 +0800

    Update grubFm.sh

--- grubFm.sh
+++ grubFm.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-git clone --depth=50 --branch=master https://github.com/a1ive/grub2-filemanager.git a1ive/grub2-filemanager
+git clone --depth=50 --branch=master https://github.com/a1ive/grub2-filemanager.git grub2-filemanager
 git submodule update --init --recursive
 sudo apt-get install -y gettext grub2-common genisoimage p7zip-full
+cd grub2-filemanager
 ./release.sh
