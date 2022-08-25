339506e0e1921aa2472cddf753475b18fdbbd871
lsq <lsqypj@gmail.com>
2022-08-25 09:20:52 +0800

    Delete grub2FM.sh

--- grub2FM.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-git clone --depth=50 --branch=master https://github.com/a1ive/grub2-filemanager.git a1ive/grub2-filemanager
-git submodule update --init --recursive
-sudo apt-get install -y gettext grub2-common genisoimage p7zip-full
-./release.sh
